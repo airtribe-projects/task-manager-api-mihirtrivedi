f1f6a661c681c392ef74514e07c28aa74165f377
Mihir Trivedi <mihirtrivedi2561993@gmail.com>
2026-03-02 08:47:27 +0530

    ihir

--- test/auth.test.js
+++ test/auth.test.js
@@ -1,6 +1,6 @@
 const request = require('supertest');
 const express = require('express');
-const userRoutes = require('../src/routes/userRoutes');
+const userRoutes = require('./src/routes/userRoutes');
 
 const app = express();
 app.use(express.json());
